d2f917d0aa04de920b660e7026584984595e9c10
Duncan Brown <duncanjbrown@gmail.com>
2019-12-16 23:41:23 +0000

    Day 6

---
+++ src/aoc2019/6.clj
@@ -0,0 +1,44 @@
+(ns aoc2019.6
+  (:require [clojure.edn :as edn]
+            [aoc2019.utils :refer :all]))
+
+(defn parse-input
+  []
+  (->> (read-file "6.txt")
+       (map #(clojure.string/split % #"\)"))
+       (map reverse)))
+
+(defn input->adj-list
+  [input]
+  (reduce (fn [acc [a b]]
+              (assoc acc a b)) {} input))
+
+(defn build-path
+  ([paths from]
+   (build-path paths from "COM"))
+  ([paths from to]
+   (let [next-orbit (get paths from)]
+     (if (= to next-orbit)
+       (seq [to])
+       (conj (build-path paths next-orbit to) next-orbit)))))
+
+(def adj-list (-> (parse-input) input->adj-list))
+
+(defn part1
+  [adj-list]
+  (let [nodes (keys adj-list)]
+    (reduce (fn [acc nxt]
+              (+ acc (count (build-path adj-list nxt)))) 0 nodes)))
+
+(part1 adj-list)
+
+(defn part2
+  [adj-list]
+  (let [you-to-com (build-path adj-list "YOU")
+        san-to-com (build-path adj-list "SAN")
+        origin (first you-to-com)
+        target (first san-to-com)
+        isec (first (drop-while (fn [x] (not-any? #(= x %) san-to-com)) you-to-com))]
+   (reduce + (map count [(build-path adj-list origin isec) (build-path adj-list target isec)]))))
+
+(part2 adj-list)
