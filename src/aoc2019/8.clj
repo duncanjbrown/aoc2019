133d844931df1253ea267b91bc6153cccc0bedc3
Duncan Brown <duncanjbrown@gmail.com>
2019-12-23 17:28:03 +0000

    Day 8.2

--- src/aoc2019/8.clj
+++ src/aoc2019/8.clj
@@ -13,9 +13,38 @@
         layer-size (* width height)]
     (partition layer-size input)))
 
-(->> (layers input)
-  (map frequencies)
-  (sort-by #(get % 0))
-  (first)
-  ((juxt #(get % 1) #(get % 2)))
-  (reduce *))
+(defn part1
+  []
+  (->> (layers input)
+    (map frequencies)
+    (sort-by #(get % 0))
+    (first)
+    ((juxt #(get % 1) #(get % 2)))
+    (reduce *)))
+
+(defn pixel-on-pixel
+  [plo phi]
+  (if (= phi 2) plo phi))
+
+(defn layer-on-layer
+  [l1 l2]
+  (map pixel-on-pixel l1 l2))
+
+(defn tr
+  [a b x]
+  (if (= x a)
+    b
+    x))
+
+(defn print-line
+  [line]
+  (->> (map (partial tr 0 " ") line)))
+
+(defn part2
+  []
+  (->> (layers input)
+    (reverse)
+    (reduce layer-on-layer)
+    (partition 25)
+    (map print-line)
+    (map println)))
